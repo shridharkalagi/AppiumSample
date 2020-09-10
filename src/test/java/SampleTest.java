bc89d589f55a201f3c6553190ae4a08fd9f879ee
saikrishna321 <saikrishna321@yahoo.com>
2020-09-10 12:07:58 +0530

    gesture test

--- src/test/java/SampleTest.java
+++ src/test/java/SampleTest.java
@@ -2,7 +2,13 @@ import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileBy;
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.AutomationName;
 import io.appium.java_client.remote.MobileCapabilityType;
+import org.openqa.selenium.Point;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Pause;
+import org.openqa.selenium.interactions.PointerInput;
+import org.openqa.selenium.interactions.Sequence;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterClass;
@@ -11,7 +17,11 @@ import org.testng.annotations.Test;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.time.Duration;
+import java.util.Collections;
 
+import static java.time.Duration.*;
+import static java.util.Collections.*;
 import static org.openqa.selenium.support.ui.ExpectedConditions.*;
 
 
@@ -24,15 +34,36 @@ public class SampleTest {
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "Android Emulator");
         capabilities.setCapability(MobileCapabilityType.NEW_COMMAND_TIMEOUT, 700000);
-        capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UIAutomator2");
+        capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, AutomationName.ANDROID_UIAUTOMATOR2);
         capabilities.setCapability(MobileCapabilityType.APP, System.getProperty("user.dir") + "/VodQA.apk");
         driver = new AndroidDriver<MobileElement>(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
     }
 
     @Test
-    public void SampleTest() {
+    public void SampleTest() throws InterruptedException {
         wait = new WebDriverWait(driver, 30);
         wait.until(presenceOfElementLocated(MobileBy.AccessibilityId("login"))).click();
+        wait.until(presenceOfElementLocated(MobileBy.AccessibilityId("slider1")));
+        driver.findElementByAccessibilityId("slider1").click();
+        WebElement slider = wait.until(presenceOfElementLocated(MobileBy.AccessibilityId("slider")));
+
+        Point location = slider.getLocation();
+        PointerInput finger = new PointerInput(PointerInput.Kind.TOUCH, "finger");
+        Sequence sequence = new Sequence(finger, 1);
+        sequence.addAction(finger.createPointerMove(ofMillis(0),
+            PointerInput.Origin.viewport(), location.x, location.y));
+        sequence.addAction(finger.createPointerDown(PointerInput.MouseButton.MIDDLE.asArg()));
+
+        sequence.addAction(new Pause(finger, ofMillis(600)));
+
+        sequence.addAction(finger.createPointerMove(ofMillis(600),
+            PointerInput.Origin.viewport(), location.x + 300, location.y));
+
+        sequence.addAction(finger.createPointerUp(PointerInput.MouseButton.MIDDLE.asArg()));
+
+        driver.perform(singletonList(sequence));
+        Thread.sleep(5000);
+
     }
 
     @AfterClass
